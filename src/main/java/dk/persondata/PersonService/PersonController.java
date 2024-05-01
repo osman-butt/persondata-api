3f19fd43bfcedc896211f95e341575af753b5088
osman-butt <butt.osman@gmail.com>
2024-05-01 15:07:04 +0200

    Create PersonController.java

---
+++ src/main/java/dk/persondata/PersonService/PersonController.java
@@ -0,0 +1,23 @@
+package dk.persondata.PersonService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @GetMapping
+    public Mono<Person> getPerson(@RequestParam String name) {
+        return personService.getPerson(name);
+    }
+
+}
